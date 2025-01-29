2c0d65b565c9c35be7295052e39676e98b5a8a32
unknown <ttlhapane88@gmail.com>
2025-01-29 23:52:25 +0200

    get_weather function

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 from geopy.geocoders import Nominatim
+latitude=0
+longitude=0
+parameters={}
 
 geolocator = Nominatim(user_agent="weatherApp") #this is a geocoding service that is part of OpenStreetMap
 city = input("Please enter your city: ")
-url = "https://www.google.com/search?q="+city+" nelongitude and latitude"
-html = requests.get(url).content
+map_url = "https://www.google.com/search?q="+city+" longitude and latitude"
+html = requests.get(map_url).content
 
 soup = BeautifulSoup(html, 'html.parser')
 
 
-
-def get_lat_lon(city):
+def get_lat_long(city):
     location = geolocator.geocode(city)
     if location:
-        return location.latitude, location.longitude
+        return round(location.latitude,2), round(location.longitude,2)
     else:
         return None, None
 
+def get_current_weather():
+    url="https://api.open-meteo.com/v1/forecast"
+    parameters={
+        "latitude":latitude,
+        "longitude": longitude,
+        "hourly":"temperature_2m"
+        
+         }
+    response=requests.get(url,params=parameters)
+    return response.json()
+    
+    
 
-latitude, longitude = get_lat_lon(city)
-print(f"{longitude}, {latitude}")
+latitude, longitude = get_lat_long(city)
 
-# def get_temperature():
+weather=get_current_weather()
+print(weather)
+
+# def get_weather():
     
